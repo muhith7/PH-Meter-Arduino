65abe408e09e452625286ea20a2babc58029213b
muhith7 <abdulmuhithnawawi@gmail.com>
2023-10-03 09:47:42 +0700

    New Project

---
+++ PHMeterArduino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ph.meter.arduino;
+
+/**
+ *
+ * @author abdul
+ */
+public class PHMeterArduino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
